26fac55778e9c6834b8f62e414d68bcb4cebce43
fireyoshiqc <fire_yoshi_qc@hotmail.com>
2018-11-09 20:03:30 -0500

    Fini personnes, objets à retravailler.

--- tp2/code/exercice4-objets.pl
+++ tp2/code/exercice4-objets.pl
@@ -1,86 +1,117 @@
-:- dynamic oui/1, non/1.
+:- dynamic oui/2, non/2.
+
+liste(X, list) :- findall(X, X, list).
 
 verifier(X, Y) :-
-    (oui(X) -> true; (non(X) -> false; ask(X, Y))).
+    (oui(X, Y) -> true; (non(X, Y) -> false; ask(X, Y))).
 
-verifier_action(X, Y) :-
-    (oui(Y) -> true; (non(Y) -> false; ask(X, Y))).
-
-ask(electrique, X) :-
-    format('Ton objet est-il electrique ? ', [X]),
+ask(X, Y) :-
+    format('~w de ton objet est : ~w ? ', [X, Y]),
     read(Reponse),
-    (Reponse = 'oui' -> assert(oui(electrique)); assert(non(electrique)), false).
-
-ask(action, Y) :-
-    format('Ton objet sert-il a ~w ? ', [Y]),
-    read(Reponse),
-    (Reponse = 'oui' -> assert(oui(Y)); assert(non(Y)), false).
-
-ask(lieu, Y) :-
-    format('~w se trouve dans le/la ? ', [Y]),
-    read(Reponse),
-    Reponse = 'oui'.
+    (Reponse = 'oui' -> assert(oui(X, Y))); assert(non(X, Y)), false.
 
 objet(_) :- undo, fail.
-objet(X) :- electromenager(X).
-objet(X) :- meuble(X).
-objet(X) :- ustensile(X).
-objet(X) :- plante(X).
+objet(X):-
+    est_alimente_par(X, _),
+    est_de_taille(X, _),
+    sert_a(X, _),
+    se_trouve(X, _).
+
+portable(X) :- (est_alimente_par(X, electrique); est_alimente_par(X, inerte)), (est_de_taille(X, petit); est_de_taille(X, moyen)), se_trouve(X, nimporte_ou).
+appareil_electromenager(X) :- est_alimente_par(X, electrique), (se_trouve(X, maison); se_trouve(X, cuisine)), (sert_a(X, nettoyer); sert_a(X, cuisiner); sert_a(X, faire_du_cafe); sert_a(X, cuire)).
+gros_electromenager(X) :- appareil_electromenager(X), est_de_taille(X, gros).
+petit_electromenager(X) :- appareil_electromenager(X), est_de_taille(X, moyen). % petit est vraiment pour les petits objets comme une fourchette
+electronique(X) :- \+ appareil_electromenager(X), est_alimente_par(X, electrique), (est_de_taille(X, petit); est_de_taille(X, moyen)), (sert_a(X, appeler); sert_a(X, travailler); sert_a(X, jouer)).
+ustensile(X) :- est_alimente_par(X, inerte), (se_trouve(X, salle_a_manger); se_trouve(X, cuisine)), (est_de_taille(X, petit); est_de_taille(X, moyen)), (sert_a(X, manger); sert_a(X, cuisiner)).
+meuble(X) :- est_alimente_par(X, inerte), (est_de_taille(X, moyen); est_de_taille(X, gros)).
+plante(X) :- est_alimente_par(X, vivant), sert_a(X, decorer).
+
+est_alimente_par(X, Y) :-
+    alimentation(Y),
+    cobjet(X, Y, _, _, _),
+    (verifier(alimentation, Y) -> foreach(alimentation(Z), assert(non(alimentation, Z)))).
+
+est_de_taille(X, Y) :-
+    taille(Y),
+    cobjet(X, _, Y, _, _),
+    (verifier(taille, Y) -> foreach(taille(Z), assert(non(taille, Z)))).
+sert_a(X, Y) :-
+    action(Y),
+    cobjet(X, _, _, Y, _),
+    (verifier(action, Y) -> foreach(action(Z), assert(non(action, Z)))).
+se_trouve(X, Y) :-
+    lieu(Y),
+    cobjet(X, _, _, _, Y),
+    (verifier(lieu, Y) -> foreach(lieu(Z), assert(non(lieu, Z)))).
 
 
-%objet(X) :- electronique(X).
-
-%objet(X) :- instrument(X).
-%objet(X) :- outil(X).
-
-%objet(X) :- meuble(X).
-
-
-electromenager(X) :- est_electrique(X), (sert_a(X, nettoyer); sert_a(X, cuisiner)).
-
-meuble(X) :- \+ est_electrique(X),
-    (sert_a(X, manger); sert_a(X, dormir); sert_a(X, contenir); sert_a(X, decorer)).
-
-ustensile(X) :- (sert_a(X, cuisiner); sert_a(X, manger)).
-
-plante(X) :- \+ est_electrique(X), sert_a(X, decorer).
-
-est_electrique(X) :- verifier(electrique, X), electrique(X).
-sert_a(X, Y) :- utilite(X, Y), action(Y), verifier_action(action, Y).
-
-utilite(aspirateur, nettoyer).
-utilite(detergent_a_vaisselle, nettoyer).
-utilite(shampooing, nettoyer).
-utilite(table, manger).
-utilite(lit, dormir).
-utilite(sac_a_dos, contenir).
-utilite(ordinateur, travailler).
-utilite(casserole, cuisiner).
-utilite(four, cuisiner).
-utilite(cactus, decorer).
+% cobjet(nom, alimentation, taille, utilite, lieu)
+cobjet(aspirateur, electrique, moyen, nettoyer, maison).
+cobjet(ordinateur, electrique, moyen, travailler, bureau).
+cobjet(ordinateur, electrique, moyen, etudier, ecole).
+cobjet(ordinateur, electrique, moyen, jouer, maison).
+cobjet(telephone, electrique, petit, appeler, _).
+cobjet(fourchette, inerte, petit, manger, salle_a_manger).
+cobjet(balai, inerte, moyen, nettoyer, maison).
+cobjet(cactus, vivant, petit, decorer, _).
+cobjet(assiette, inerte, moyen, manger, salle_a_manger).
+cobjet(four, electrique, gros, cuire, cuisine).
+cobjet(cuisiniere, electrique, gros, cuisiner, cuisine).
+cobjet(cafetiere, electrique, moyen, faire_du_cafe, cuisine).
+cobjet(cafetiere, electrique, moyen, faire_du_cafe, bureau).
+cobjet(grille_pain, electrique, moyen, griller, cuisine).
+cobjet(table, inerte, gros, manger, salle_a_manger).
+cobjet(table, inerte, gros, travailler, bureau).
+cobjet(casserole, inerte, moyen, cuisiner, cuisine).
+cobjet(shampooing, inerte, petit, nettoyer, salle_de_bain).
+cobjet(detergent_a_vaisselle, inerte, petit, nettoyer, cuisine).
+cobjet(lit, inerte, gros, dormir, chambre).
+cobjet(cle, inerte, petit, deverrouiller, poche).
+cobjet(portefeuille, inerte, petit, payer, poche).
+cobjet(sac_a_dos, inerte, moyen, transporter, ecole).
+cobjet(sac_a_dos, inerte, moyen, transporter, bureau).
+cobjet(piano, inerte, gros, jouer_de_la_musique, maison).
+cobjet(lampe, electrique, moyen, eclairer, maison).
+cobjet(papier, inerte, moyen, ecrire, _).
 
 
-electrique(aspirateur).
-electrique(four).
-electrique(ordinateur).
+alimentation(electrique).
+alimentation(inerte).
+alimentation(vivant).
+
+taille(petit).
+taille(moyen).
+taille(gros).
 
 lieu(cuisine).
 lieu(salle_de_bain).
 lieu(bureau).
-
+lieu(maison).
+lieu(ecole).
+lieu(partout).
+lieu(salle_a_manger).
+lieu(salle_de_bain).
+lieu(chambre).
+lieu(poche).
 
 action(manger).
 action(cuisiner).
 action(nettoyer).
 action(travailler).
-action(etudier).
 action(dormir).
-action(contenir).
-action(musique).
+action(transporter).
+action(jouer_de_la_musique).
 action(eclairer).
 action(jouer).
 action(decorer).
+action(appeler).
+action(griller).
+action(faire_du_cafe).
+action(deverrouiller).
+action(ecrire).
+action(payer).
+action(cuire).
 
-undo :- retract(oui(_)), fail.
-undo :- retract(non(_)), fail.
+undo :- retract(oui(_, _)), fail.
+undo :- retract(non(_, _)), fail.
 undo.
--- tp2/code/exercice4_personnes.pl
+++ tp2/code/exercice4_personnes.pl
@@ -1,32 +1,139 @@
-ask(statut, Y) :-
-      format('~w est en vie ? ', [Y]),
+ask(vivant, Y) :-
+      format('~w est-il en vie ? ', [Y]),
       read(Reponse),
       Reponse = 'oui'.
 ask(reel, Y) :-
-      format('~w existe ? ', [Y]),
+      format('~w est-il reel ? ', [Y]),
       read(Reponse),
       Reponse = 'oui'.
 ask(homme, Y) :-
-      format('~w est un homme ? ', [Y]),
+      format('~w est-il un homme ? ', [Y]),
       read(Reponse),
       Reponse = 'oui'.
-#si reponse = non, implique femme(Y)
+ask(chanteur, Y) :-
+      format('~w est-il un chanteur ? ', [Y]),
+      read(Reponse),
+      Reponse = 'oui'.
+ask(acteur, Y) :-
+      format('~w est-il un acteur ? ', [Y]),
+      read(Reponse),
+      Reponse = 'oui'.
+ask(ecrivain, Y) :-
+      format('~w est-il un ecrivain? ', [Y]),
+      read(Reponse),
+      Reponse = 'oui'.
+%#si reponse = non, implique femme(Y)
 ask(politicien, Y) :-
-	  format('~w est un politicien ?', [Y]),
+	  format('~w est-il un politicien ?', [Y]),
 	  read(Reponse),
 	  Reponse = 'oui'.
 ask(artiste, X) :-
-      format('~w est un artiste ? ', [X]),
+      format('~w est-il un artiste ? ', [X]),
       read(Reponse),
       Reponse = 'oui'.
-ask(religion, X) :-
-      format('~w est relie a la religion ? ', [X]),
+ask(religieux, X) :-
+      format('~w est-il relie a la religion ? ', [X]),
+      read(Reponse),
+      Reponse = 'oui'.
+ask(pilote, X) :-
+      format('~w est-il un pilote de course ? ', [X]),
+      read(Reponse),
+      Reponse = 'oui'.
+ask(noir, X) :-
+      format('~w a-t-il la peau noire ? ', [X]),
+      read(Reponse),
+      Reponse = 'oui'.
+ask(japonais, X) :-
+      format('~w est-il japonais ? ', [X]),
+      read(Reponse),
+      Reponse = 'oui'.
+ask(vrai_nom, X) :-
+      format('~w utilise-t-il son vrai nom ? ', [X]),
+      read(Reponse),
+      Reponse = 'oui'.
+ask(americain, X) :-
+      format('~w est-il americain ? ', [X]),
+      read(Reponse),
+      Reponse = 'oui'.
+ask(demissionne, X) :-
+      format('~w a-t-il demissionne ? ', [X]),
+      read(Reponse),
+      Reponse = 'oui'.
+ask(fils_dieu, X) :-
+      format('~w est-il le fils de Dieu ? ', [X]),
+      read(Reponse),
+      Reponse = 'oui'.
+ask(plombier, X) :-
+      format('~w est-il un plombier ? ', [X]),
       read(Reponse),
       Reponse = 'oui'.
 
-personne(X) :- politicien(X).
-personne(X) :- artiste(X).
-artiste(X) :- ask(chanteur, X), chanteur(X).
-artiste(X) :- ask(musicien, X), musicien(X).
-!homme(X) :- femme(X)
+personne(X) :- ask(reel, X), reel(X).
+personne(X) :- ask(homme, X), homme_jeu(X).
+personne(X) :- true, lc(X). % Lara Croft
+
+reel(X) :- ask(homme, X), homme_reel(X).
+reel(X) :- ask(artiste, X), femme_artiste(X).
+reel(X) :- true, cleo(X). % Cléopâtre
+
+homme_reel(X) :- ask(artiste, X), homme_artiste(X).
+homme_reel(X) :- ask(politicien, X), homme_politicien(X).
+homme_reel(X) :- ask(religieux, X), homme_religieux(X).
+homme_reel(X) :- ask(pilote, X), homme_pilote(X).
+
+homme_artiste(X) :- ask(noir, X), homme_artiste_noir(X).
+homme_artiste(X) :- ask(vivant, X), homme_artiste_pas_noir_vivant(X).
+homme_artiste(X) :- vh(X). % Victor Hugo
+
+homme_artiste_noir(X) :- ask(vivant, X), dz(X). % Denzel Washington.
+homme_artiste_noir(X) :- true, mj(X). % Michael Jackson
+
+homme_artiste_pas_noir_vivant(X) :- ask(japonais, X), hk(X). % Hideo Kojima.
+homme_artiste_pas_noir_vivant(X) :- ask(vrai_nom, X), qt(X). % Quentin Tarantino.
+homme_artiste_pas_noir_vivant(X) :- true, bk(X). % Banksy.
+
+homme_politicien(X) :- ask(americain, X), homme_politicien_americain(X).
+homme_politicien(X) :- ask(vivant, X), mg(X). % Mikhail Gorbachev.
+homme_politicien(X) :- staline(X). % Joseph Staline.
+
+homme_politicien_americain(X) :- ask(demissionne, X), nixon(X).
+homme_politicien_americain(X) :- eisenhower(X).
+
+homme_religieux(X) :- ask(fils_dieu, X), js(X).
+homme_religieux(X) :- ask(vivant, X), pape(X).
+homme_religieux(X) :- true, ms(X).
+
+homme_pilote(X) :- ask(vivant, X), alonso(X).
+homme_pilote(X) :- true, senna(X).
+
+femme_artiste(X) :- ask(acteur, X), jl(X). % Jennifer Lawrence
+femme_artiste(X) :- ask(ecrivain, X), jkr(X). % J.K. Rowling
+femme_artiste(X) :- true, lg(X). % Lady Gaga
+
+homme_jeu(X) :- ask(plombier, X), mr(X).
+homme_jeu(X) :- true, jb(X).
+
+lc(lara_croft).
+cleo(cleopatre).
+vh(victor_hugo).
+dz(denzel_washington).
+mj(michael_jackson).
+hk(hideo_kojima).
+bk(banksy).
+qt(quentin_tarantino).
+mg(mikhail_gorbachev).
+staline(joseph_staline).
+nixon(richard_nixon).
+eisenhower(dwight_d_eisenhower).
+js(jesus).
+pape(pape_francois).
+ms(moise).
+alonso(fernando_alonso).
+senna(ayrton_senna).
+jl(jennifer_lawrence).
+jkr(jk_rowling).
+lg(lady_gaga).
+mr(mario).
+jb(james_bond).
+
 
