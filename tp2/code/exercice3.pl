9cf40d4d0697d7cfdde0a0212f65306b8e95426f
Fabrice Dugas <fabby.dugas@gmail.com>
2018-11-09 19:30:02 -0500

    Add comments on exo3

--- tp2/code/exercice3.pl
+++ tp2/code/exercice3.pl
@@ -9,6 +9,7 @@ prerequis(log1000, log2410).
 prerequis(inf2010, inf2705).
 
 % corequis(A,B) A est un corequis de B
+% dans le contexte, un corequis est géré de la même façon quun prérequis
 prerequis(log2810, inf2010).
 prerequis(mth1007, inf2705).
 prerequis(log2990, inf2705).
@@ -16,14 +17,16 @@ prerequis(inf1600, inf1900).
 prerequis(log1000, inf1900).
 prerequis(inf2205, inf1900).
 
+% trouver tous les prérequis récursivement
 complet(Prerequis,Cours):-prerequis(Prerequis,Cours).
-
 complet(Prerequis,Cours):-prerequis(AutrePrerequis, Cours),
      complet(Prerequis, AutrePrerequis).
 
+% supprimer les duplicats grace a setof et imprimer la liste résultante
 coursAPrendreComplet(Cours):-setof(Prerequis, Prerequis^complet(Prerequis, Cours), List),
     printList(List).
 
+% imprimer une liste récursivement
 printList([]).
 printList([Head|Rest]):-
     format('~w~n', Head),
